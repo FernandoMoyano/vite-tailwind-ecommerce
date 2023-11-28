f4689fe95d0f258c8536758a182bb92a1130bcdd
FernandoMoyano <fernandomoyano21@gmail.com>
2023-11-28 17:03:00 -0300

    Routes-react-router-dom

--- src/Pages/App/App.jsx
+++ src/Pages/App/App.jsx
@@ -1,12 +1,25 @@
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "../../App.css";
+import Home from "../Home/Home";
+import MyAccount from "../MyAccount/MyAccount";
+import MyOrder from "../MyOrder/MyOrder";
+import MyOrders from "../MyOreders/MyOrders";
+import NotFound from "../NotFound/NotFound";
+import SignIn from "../SignIn/SignIn";
 
 function App() {
-   
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route to="/" element={<Home />}></Route>
+        <Route to="/my-account" element={<MyAccount />}></Route>
+        <Route to="/my-order" element={<MyOrder />}></Route>
+        <Route to="/my-orders" element={<MyOrders />}></Route>
+        <Route to="/*" element={<NotFound />}></Route>
+        <Route to="/sign-in" element={<SignIn />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
