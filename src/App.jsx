1075f9e5c604321952cc9f9a86376f3b9496c5bb
FernandoMoyano <fernandomoyano21@gmail.com>
2023-11-28 16:39:47 -0300

    tailwind-config

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+   
   return (
-    <div>Hola Mundo</div>
+    <h1 className="text-3xl font-bold underline">
+    Hello world!
+  </h1>
   )
 }
 
