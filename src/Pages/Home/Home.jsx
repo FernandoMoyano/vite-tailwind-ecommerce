dc956c3f0ffab118f302d948ed7f4d1e5bf09b38
FernandoMoyano <fernandomoyano21@gmail.com>
2023-11-29 14:54:29 -0300

    fetch(Api-Platzi)

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,31 @@
+import { useEffect, useState } from "react";
 import Card from "../../components/Card/card";
 import Layout from "../../components/Layout/Layout";
 import NavBar from "../../components/NavBar/NavBar";
 
 const Home = () => {
+  const [items, setItems] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://api.escuelajs.co/api/v1/products"
+        );
+        const data = response.json();
+        setItems(data);
+      } catch (error) {
+        console.log(`Hubo un error ${error}`);
+      }
+    };
+    fetchData();
+  }, []);
   return (
     <Layout>
       <NavBar />
-      <Card/>
+      {items.map((item) => (
+        <Card data={item} key={item.id} />
+      ))}
       Home
     </Layout>
   );
