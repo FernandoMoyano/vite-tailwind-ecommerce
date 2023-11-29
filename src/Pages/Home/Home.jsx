5be5e629c04bbecb4c1ae0d6efae12bb9d4fa307
FernandoMoyano <fernandomoyano21@gmail.com>
2023-11-29 16:04:13 -0300

    fetch(Fake Store Api)

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -9,10 +9,8 @@ const Home = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(
-          "https://api.escuelajs.co/api/v1/products"
-        );
-        const data = response.json();
+        const response = await fetch("https://fakestoreapi.com/products");
+        const data = await response.json();
         setItems(data);
       } catch (error) {
         console.log(`Hubo un error ${error}`);
@@ -23,10 +21,12 @@ const Home = () => {
   return (
     <Layout>
       <NavBar />
-      {items.map((item) => (
-        <Card data={item} key={item.id} />
-      ))}
       Home
+      <section className="grid gap'4 grid-cols-4 w-full max-w-screen-lg">
+        {items?.map((item) => (
+          <Card key={item.id} {...item} />
+        ))}
+      </section>
     </Layout>
   );
 };
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,11 +1,14 @@
-const Card = (data) => {
+/* eslint-disable react/prop-types */
+const Card = ({ price, title, image, category}) => {
   return (
     <div className="bg-white cursor-pointer w-56 h-60">
       <figure className="relative mb-2 w-full h-4/5">
-        <span className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5">{data.category.id}</span>
+        <span className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5">
+          {category}
+        </span>
         <img
           className="w-full h-full object-cover"
-          src=""
+          src={image}
           alt="headpones"
         ></img>
         <button className="absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1">
@@ -13,8 +16,8 @@ const Card = (data) => {
         </button>
       </figure>
       <p className="flex justify-between">
-        <span className="text-sm font-light">Headpones</span>
-        <span className="text-md font-medium">$300</span>
+        <span className="text-sm font-light truncate mr-2">{title}</span>
+        <span className="text-md font-medium">{price}</span>
       </p>
     </div>
   );
